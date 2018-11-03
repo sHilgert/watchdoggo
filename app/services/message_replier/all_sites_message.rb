4b3d50fd050139052a4d770fa44c45229ed6f642
Sérgio Hilgert Junior <hilgertfacul@gmail.com>
2018-11-03 18:37:03 -0300

    add comma between sites list

--- app/services/message_replier/all_sites_message.rb
+++ app/services/message_replier/all_sites_message.rb
@@ -1,7 +1,7 @@
 module MessageReplier
   class AllSitesMessage < BaseMessage
     def reply
-      sites = Site.enabled.pluck(:url).join('\n')
+      sites = Site.enabled.pluck(:url).join(', ')
       bot.api.send_message(chat_id: chat.id, text: "to olhando esses sites aqui:\n#{sites}")
     end
   end
