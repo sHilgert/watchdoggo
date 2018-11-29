41c62b546256763574b235b7ee16f7d030f1e493
sergio.hilgert <hilgertfacul@gmail.com>
2018-11-29 10:35:35 -0200

    add executors

--- config/initializers/bot.rb
+++ config/initializers/bot.rb
@@ -1,9 +1,11 @@
 require 'telegram/bot'
 
 Thread.new do
-  Telegram::Bot::Client.run(Settings.telegram_token) do |bot|
-    bot.listen do |message|
-      MessageReplier::Factory.for(message, bot).reply
+  Rails.application.executor.wrap do
+    Telegram::Bot::Client.run(Settings.telegram_token) do |bot|
+      bot.listen do |message|
+        MessageReplier::Factory.for(message, bot).reply
+      end
     end
   end
 end
--- config/initializers/monitor.rb
+++ config/initializers/monitor.rb
@@ -2,23 +2,25 @@ require 'telegram/bot'
 require 'http'
 
 Thread.new do
-  while true
-    Site.enabled.find_each do |site|
-      begin
-        res = HTTP.get(site.url)
-        if res.code >= 400
-          Response.create!(site: site, http_code: res.code)
-          site.users.find_each do |user|
-            Telegram::Bot::Api.new(Settings.telegram_token).send_message(
-              chat_id: user.external_id,
-              text: "Ixi, deu treta no #{site.url}, CODE: #{res.code}"
-            )
+  Rails.application.executor.wrap do
+    while true
+      Site.enabled.find_each do |site|
+        begin
+          res = HTTP.get(site.url)
+          if res.code >= 400
+            Response.create!(site: site, http_code: res.code)
+            site.users.find_each do |user|
+              Telegram::Bot::Api.new(Settings.telegram_token).send_message(
+                chat_id: user.external_id,
+                text: "ixi, deu treta no #{site.url}, CODE: #{res.code}"
+              )
+            end
           end
+        rescue StandardError => error
+          Rails.logger.error(error.to_s)
         end
-      rescue StandardError => error
-        Rails.logger.error(error.to_s)
       end
+      sleep(1.minute)
     end
-    sleep(1.minute)
   end
 end
