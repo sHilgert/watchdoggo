4a014bc3bdbe6c414e53ebcab08110c2a0e05539
sergio.hilgert <hilgertfacul@gmail.com>
2018-11-29 10:48:29 -0200

    remove thread

--- config/initializers/bot.rb
+++ config/initializers/bot.rb
@@ -1,11 +1,7 @@
 require 'telegram/bot'
 
-Thread.new do
-  Rails.application.executor.wrap do
-    Telegram::Bot::Client.run(Settings.telegram_token) do |bot|
-      bot.listen do |message|
-        MessageReplier::Factory.for(message, bot).reply
-      end
-    end
+Telegram::Bot::Client.run(Settings.telegram_token) do |bot|
+  bot.listen do |message|
+    MessageReplier::Factory.for(message, bot).reply
   end
 end
